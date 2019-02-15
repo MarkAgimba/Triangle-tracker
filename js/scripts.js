481696a8ea09b52556622e154fc989317a8d27d1
Mark Agimba <markaguero16@gmail.com>
2019-02-15 12:50:40 +0300

    index

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function check_getTriangles(){
+  var sideA = parseFloat(prompt("Enter Parameters for side A"));
+  var sideB = parseFloat(prompt("Enter Parameters for side B"));
+  var sideC = parseFloat(prompt("Enter Parameters for side C"));
+
+if(sideA===sideB && sideB===sideC && sideC===sideB){
+alert("Triangle is Equilateral ");
+}
+else if(sideA===sideB || sideA===sideC || sideC===sideB){
+alert("Triangle is Isosceles");
+}
+else if((sideA+sideB)<= sideC || (sideA+sideC)<=sideB || (sideB+sideC)<=sideA)
+alert("Values don't match Triangle");
+}
+else{
+  alert("Triangle is Scalene");
+}
+}
