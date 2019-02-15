29799e7fbe7a4557a454c9bcfed19c34868bc9ea
Mark Agimba <markaguero16@gmail.com>
2019-02-15 11:34:19 +0300

    script function var

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ function check_getTriangles(){
   var sideA = parseFloat(prompt("Enter Parameters for side A"));
   var sideB = parseFloat(prompt("Enter Parameters for side B"));
   var sideC = parseFloat(prompt("Enter Parameters for side C"));
+  
