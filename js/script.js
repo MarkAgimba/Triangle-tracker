f10911414e2ae409eb8fa98f5e2429c4ea15fb8a
Mark Agimba <markaguero16@gmail.com>
2019-02-15 11:37:57 +0300

    if Equilateral

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ function check_getTriangles(){
   var sideA = parseFloat(prompt("Enter Parameters for side A"));
   var sideB = parseFloat(prompt("Enter Parameters for side B"));
   var sideC = parseFloat(prompt("Enter Parameters for side C"));
-  
+
+if(sideA===sideB && sideB===sideC && sideC===sideB){
+alert("Triangle is Equilateral ");
+}
