b5bc72be5eb922087951df332f9e8ce995a2e2e9
Mark Agimba <markaguero16@gmail.com>
2019-02-21 12:53:50 +0300

    js for equilateral/not a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ function trackButton() {
   var A=parseFloat(document.getElementById("A").value);
   var B=parseFloat(document.getElementById("B").value);
   var C=parseFloat(document.getElementById("C").value);
-
-  if (A === B && B === C && B === C) {
+  if((A+B)<=C || (B+C)<=A || (C+C)<=A){
+    document.getElementById("result").innerHTML="This is not a triangle"
+  }else if ((A===B && (B === C) && (C===A)){
     document.getElementById("result").innerHTML="This is an Equilateral triangle"
   } else if(A === B || A === C || C === B) {
     document.getElementById("result").innerHTML="This is an Isosceles Triangle"
-  } else if((A + B) <= C || (A + C) <= B || (B + C) <= A) {
-    document.getElementById("result").innerHTML="This is a Scalene triangle"
+
   } else {
     (A !== B) >= C || (B + C) >= A || (C + A) >= B
     document.getElementById("result").innerHTML="This isnt a Triangle"
