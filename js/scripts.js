4d53468de0bd5084f02ef2817f71169ccbb0e9ee
Mark Agimba <markaguero16@gmail.com>
2019-02-21 15:10:20 +0300

    brackets missing for js formulas

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 function trackButton() {
-  var A=parseFloat(document.getElementById("A").value);
-  var B=parseFloat(document.getElementById("B").value);
-  var C=parseFloat(document.getElementById("C").value);
-  if((A+B)<=C || (B+C)<=A || (C+C)<=A){
+  var A=parseFloat(document.getElementById("a").value);
+  var B=parseFloat(document.getElementById("b").value);
+  var C=parseFloat(document.getElementById("c").value);
+  if((A+B)<=C || (B+C)<=A || (C+A)<=B){
     document.getElementById('result').innerHTML="This is not a triangle"
-  } else if ((A===B && (B === C) && (C===A)){
+  } else if ((A===B) && (B === C) && (C===A)) {
     document.getElementById('result').innerHTML="This is an Equilateral triangle"
-  } else if((A===B) || (B===C) || (C === B) {
+  } else if ((A===B) || (B===C) || (C === B)) {
     document.getElementById('result').innerHTML="This is an Isosceles Triangle"
-  } else if((A!==B) && (B!==C) && (C!==A)) {
+  }else if ((A!==B) && (B!==C) && (C!==A)) {
     document.getElementById('result').innerHTML="This is a scalene triangle"
+  }else{
+    document.getElementById('result').innerHTML="Enter the values"
   }
 };
