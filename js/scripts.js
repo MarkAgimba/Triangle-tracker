a535ba3d7102b91516578d5c4b4d1594b067607a
Mark Agimba <markaguero16@gmail.com>
2019-02-21 12:48:51 +0300

    parseFloat input

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
-function trackButton(){
-  var A=document.getElementById("A").value;
-  var B=document.getElementById("B").value;
-  var C=document.getElementById("C").value;
+function trackButton() {
 
-  if(A===B && B===C && B===C){
-document.getElementById("result").innerHTML="This is an Equilateral triangle"
+  var A=parseFloat(document.getElementById("A").value);
+  var B=parseFloat(document.getElementById("B").value);
+  var C=parseFloat(document.getElementById("C").value);
+
+  if (A === B && B === C && B === C) {
+    document.getElementById("result").innerHTML="This is an Equilateral triangle"
+  } else if(A === B || A === C || C === B) {
+    document.getElementById("result").innerHTML="This is an Isosceles Triangle"
+  } else if((A + B) <= C || (A + C) <= B || (B + C) <= A) {
+    document.getElementById("result").innerHTML="This is a Scalene triangle"
+  } else {
+    (A !== B) >= C || (B + C) >= A || (C + A) >= B
+    document.getElementById("result").innerHTML="This isnt a Triangle"
   }
-  else if(A===B || A===C || C===B){
-document.getElementById("result").innerHTML="This is an Isosceles Triangle"
-  }
-  else if((A+B)<= C || (A+C)<=B || (B+C)<=A) {
-document.getElementById("result").innerHTML="This isn't a Triangle"
-  }
-  else{(A+B)>=C || (B+C)>=A || (C+A)>=B
-document.getElementById("result").innerHTML="This is a Scalene Triangle"
-  }
-  }
+}
