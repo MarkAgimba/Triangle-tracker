45fcefd9210371fd687670f5795feb1c1f3cdcaf
Mark Agimba <markaguero16@gmail.com>
2019-02-21 13:00:51 +0300

    js for iscosceles and scalene

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 function trackButton() {
-
   var A=parseFloat(document.getElementById("A").value);
   var B=parseFloat(document.getElementById("B").value);
   var C=parseFloat(document.getElementById("C").value);
   if((A+B)<=C || (B+C)<=A || (C+C)<=A){
-    document.getElementById("result").innerHTML="This is not a triangle"
-  }else if ((A===B && (B === C) && (C===A)){
-    document.getElementById("result").innerHTML="This is an Equilateral triangle"
-  } else if(A === B || A === C || C === B) {
-    document.getElementById("result").innerHTML="This is an Isosceles Triangle"
-
-  } else {
-    (A !== B) >= C || (B + C) >= A || (C + A) >= B
-    document.getElementById("result").innerHTML="This isnt a Triangle"
+    document.getElementById('result').innerHTML="This is not a triangle"
+  } else if ((A===B && (B === C) && (C===A)){
+    document.getElementById('result').innerHTML="This is an Equilateral triangle"
+  } else if((A===B) || (B===C) || (C === B) {
+    document.getElementById('result').innerHTML="This is an Isosceles Triangle"
+  } else if((A!==B) && (B!==C) && (C!==A)) {
+    document.getElementById('result').innerHTML="This is a scalene triangle"
   }
-}
+};
